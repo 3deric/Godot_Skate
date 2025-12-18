a9c530fe60a7a13ee65b713d4b8910d5279e25d6
Eric <eric.schubert.3d@gmail.com>
2025-12-18 15:12:19 +0100

    Set states as global, improved trick controller

--- Skategame/Scripts/character_tricks.gd
+++ Skategame/Scripts/character_tricks.gd
@@ -1,36 +1,57 @@
 class_name CharacterTricks
 extends Node3D
 
+const COMBO_COOLDOWN_TIME : float = 0.2
+
 var curr_trick_rot : float = 0.0
 var curr_trick_time : float = 0.0
+var curr_trick_state : CharStates.State = CharStates.State.RESET
+var combo_cooldown : float = 0.0
 var tricks : Dictionary = {}
 var is_trick : bool = false
+
+@onready var Char_Input : CharacterInput = $"../Char_Input"
+@onready var Char : CharacterController = $".."
 	
 func _ready() -> void:
 	pass
 	
 	
 func _process(delta: float) -> void:
-	pass
+	if !is_trick:
+		return
+	if curr_trick_state == CharStates.State.AIR or\
+		curr_trick_state == CharStates.State.PIPESNAP or\
+		curr_trick_state == CharStates.State.PIPESNAPAIR:
+		curr_trick_rot += Char_Input.get_input().x * Char.ROT_JUMP
+	if combo_cooldown > 0.0:
+		combo_cooldown -= delta
 		
 		
 func set_start_trick(state : CharStates.State) -> void:
-	if is_trick:
+	if is_trick or combo_cooldown > 0.01:
+		_set_append_trick(state)
 		print("appending trick!")
 	else:
 		print("starting trick!")
 	is_trick = true
+	curr_trick_state = state
 	
 	
 func set_end_trick(state : CharStates.State) -> void:
 	if state == CharStates.State.FALL:
+		_reset_trick_rot()
 		print("failed trick!")
 	else:
-		print("ending trick!")
+		print("ending trick! with rot: " + str(abs(curr_trick_rot)))
+		_reset_trick_rot()
 	is_trick = false
+	curr_trick_state = CharStates.State.RESET
+	combo_cooldown = COMBO_COOLDOWN_TIME
 		
 func _set_append_trick(state : CharStates.State) -> void:
-	pass
+	print("ending trick! with rot: " + str(abs(curr_trick_rot)))
+	curr_trick_rot = 0
 
 func set_trick_rot(_delta : float) -> void:
 	curr_trick_rot += _delta
