8eb4cd3af1eb1c836f0a79ffbf9de2028f08d969
Eric <eric.schubert.3d@gmail.com>
2026-07-17 21:29:39 +0200

    Implemented basic game loop, need to add level loop next

--- Skategame/Scripts/Game/main_menu.gd
+++ Skategame/Scripts/Game/main_menu.gd
@@ -15,7 +15,6 @@ extends Control
 var main_game : MainGame = null
 
 func init(main : MainGame) -> void:
-	main_game = main
 	_setup_buttons()
 	set_visibility(false)
 
@@ -23,18 +22,18 @@ func set_visibility(_vis : bool):
 	self.visible = _vis
 
 func _setup_buttons() -> void:
-	button_start.button_down.connect(_on_button_start_pressed)
-	button_start_2.button_down.connect(_on_button_start_2_pressed)
+	#button_start.button_down.connect()
+	#button_start_2.button_down.connect()
 	button_options.button_down.connect(_on_button_options_pressed)
 	button_customization.button_down.connect(_on_button_customization_pressed)
 	button_back_options.button_down.connect(_on_button_back_options_pressed)
 	button_back_customization.button_down.connect(_on_button_back_customization_pressed)
 
-func _on_button_start_pressed() -> void:
-	main_game.load_level(main_game.LEVEL_1_UID)
-
-func _on_button_start_2_pressed() -> void:
-	main_game.load_level(main_game.LEVEL_2_UID)
+#func _on_button_start_pressed() -> void:
+	#pass
+	#
+#func _on_button_start_2_pressed() -> void:
+	#pass
 
 func _on_button_options_pressed() -> void:
 	panel_main.visible = false
