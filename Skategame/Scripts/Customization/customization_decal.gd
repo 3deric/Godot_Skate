2787e698fda0e0cbb322bcbbb245196bc9b88bbc
Eric <eric.schubert.3d@gmail.com>
2026-08-24 19:58:54 +0200

    Added first customization ressources for the new system, updated ressource datastructure

--- Skategame/Scripts/Customization/customization_decal.gd
+++ Skategame/Scripts/Customization/customization_decal.gd
@@ -1,9 +1,9 @@
 extends Resource
-class_name CustomizationAsset
+class_name CustomizationDecal
 
 @export var name 		 : String
 @export var display_name : String
 @export var thumbnail	 : Texture2D
 @export var part    	 : CustomizationPart.Part
-@export var ressource 	 : ArrayMesh
+@export var ressource 	 : Texture2D
 @export var description  : String
--- Skategame/Scripts/Customization/customization_decals.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name CustomizationDecal
-
-@export var name 		: String
-@export var thumbnail	: Texture2D
-@export var part    	: CustomizationPart.Part
-@export var ressource 	: Texture2D
-@export var description : String
